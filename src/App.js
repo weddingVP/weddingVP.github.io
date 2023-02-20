37bfbe07232454bde9d513689b7fe581e76697c6
Anastasya Opredelentseva <nastyaop@mail.ru>
2023-02-20 21:32:39 +0300

    Creatte timer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import BannerComponent from "./component/BannerComponent";
+import TimerComponent from "./component/TimerComponent";
 import "./font/sweet.ttf";
 
 function App() {
   return (
     <div className='App'>
       <BannerComponent />
+      <TimerComponent />
     </div>
   );
 }
